89363e566918a49e9c9971c4742bf41838e4a585
Darren <darren@specno.com>
2021-04-13 20:29:33 +0200

    Remove import from landing module and scroll component from landing page component

--- src/app/pages/landing/landing.module.ts
+++ src/app/pages/landing/landing.module.ts
@@ -5,10 +5,9 @@ import { LandingComponent } from './landing.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 import { LandingRoutingModule } from './landing-routing.module';
-import { ScrollModule } from 'src/app/shared/scroll/scroll.component';
 
 @NgModule({
   declarations: [LandingComponent, LandingPageComponent],
-  imports: [CommonModule, LandingRoutingModule, ScrollModule],
+  imports: [CommonModule, LandingRoutingModule],
 })
 export class LandingModule {}

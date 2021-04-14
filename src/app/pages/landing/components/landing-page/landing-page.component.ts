88c92714cbcff028bf4425e17abf73537b5f38a8
Darren <darren@specno.com>
2021-04-14 07:41:00 +0200

    Implement emoji animation, establish function naming convention
    Functions called from FE will be prefixed by perform, other functions suffixed with helper

--- src/app/pages/landing/components/landing-page/landing-page.component.ts
+++ src/app/pages/landing/components/landing-page/landing-page.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent implements OnInit {
+  animateEmoji: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  performAnimateEmoji() {
+    this.animateEmoji = true;
+    setTimeout(() => {
+      this.animateEmoji = false;
+    }, 800);
   }
-
 }

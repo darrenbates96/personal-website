6126b937c2ab1c4ccc848b6702a5b230361fec91
Darren <darren@specno.com>
2021-04-14 06:42:02 +0200

    Slightly alter dimensions of scroll icon, include scroll module in landing page

--- src/app/pages/landing/landing.module.ts
+++ src/app/pages/landing/landing.module.ts
@@ -5,9 +5,10 @@ import { LandingComponent } from './landing.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 import { LandingRoutingModule } from './landing-routing.module';
+import { ScrollModule } from 'src/app/shared/scroll/scroll.component';
 
 @NgModule({
   declarations: [LandingComponent, LandingPageComponent],
-  imports: [CommonModule, LandingRoutingModule],
+  imports: [CommonModule, LandingRoutingModule, ScrollModule],
 })
 export class LandingModule {}

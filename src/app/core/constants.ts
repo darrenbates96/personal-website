a3fd9a1852e2e41bc3cdd261ceebe46baa9d8d45
Darren <darren@specno.com>
2021-05-29 16:03:19 +0200

    Extract about page text to constants file, shorten scroll-component animation duration

--- src/app/core/constants.ts
+++ src/app/core/constants.ts
@@ -3,3 +3,9 @@ export const landingPageText: string[] = [
   "I'm a software engineer...",
   "Let's bring your ideas to life!",
 ];
+
+export const aboutComponentText: string[] = [
+  'I love to solve technical problems in creative ways by building user experiences.',
+  "I've been educated in how to effectively and securely manage and manipulate information of any kind, but my passion is building web and mobile apps.",
+  'I can use this combination of passion and experience to build journeys for your users that solve complex problems in efficient, but delightful ways.',
+];
--- src/app/pages/landing/components/about/about.component.ts
+++ src/app/pages/landing/components/about/about.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { aboutComponentText } from 'src/app/core/constants';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
+  text: string[] = aboutComponentText;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
